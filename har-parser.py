3b9d646a4918cda69fcfd0350a591d90f1b45e07
as3284 <as3284@cornell.edu>
2020-10-08 12:35:32 -0400

    simplifying code

--- har-parser.py
+++ har-parser.py
@@ -1,12 +1,17 @@
 import json
 import webbrowser
+import random
+import time
+from os import listdir
 
-posts = ['2.14.20.json', '11.29.19.json', '11.19.19.json', '11.6.19.json', '11.2.19.json', '10.31.19.json',
-         '10.28.19.json', '10.10.19.json', '10.3.19.json', '10.1.19.json', '9.29.19.json', '9.1.19.json',
-         '8.25.19.json', '8.15.19.json', '8.6.19.json', '7.23.19.json', '7.7.19.json', '5.31.19.json',
-         '5.25.19.json', '5.18.19.json', '5.12.19.json', '5.2.19.json', '4.22.19.json', '4.17.19.json',
-         '4.3.19.json', '4.1.19.json', '3.19.19.json', '3.15.19.json', '1.19.19.json', '1.12.19.json',
-         '12.31.18.json', '12.24.18.json', '12.22.18.json', '12.2.18.json', '11.9.18.json', '11.3.18.json']
+posts = listdir("../posts-data")
+
+# ['2.14.20.json', '11.29.19.json', '11.19.19.json', '11.6.19.json', '11.2.19.json', '10.31.19.json',
+#      '10.28.19.json', '10.10.19.json', '10.3.19.json', '10.1.19.json', '9.29.19.json', '9.1.19.json',
+#      '8.25.19.json', '8.15.19.json', '8.6.19.json', '7.23.19.json', '7.7.19.json', '5.31.19.json',
+#      '5.25.19.json', '5.18.19.json', '5.12.19.json', '5.2.19.json', '4.22.19.json', '4.17.19.json',
+#      '4.3.19.json', '4.1.19.json', '3.19.19.json', '3.15.19.json', '1.19.19.json', '1.12.19.json',
+#      '12.31.18.json', '12.24.18.json', '12.22.18.json', '12.2.18.json', '11.9.18.json', '11.3.18.json']
 
 #      '10.31.18.json', '10.16.18.json', '10.6.18.json', '9.30.18.json', '9.24.18.json', '9.17.18.json',
 #      '9.6.18.json', '9.4.18.json', '8.28.18.json', '8.26.18.json', '8.24.18.json', '8.23.18.json',
@@ -32,8 +37,8 @@ def har_parser(post_list):
     likers = []
 
     for post in post_list:
-        with open('posts-data/' + post) as f:
-            data = json.load(f)
+        with open('posts-data/' + post) as k:
+            data = json.load(k)
 
         query_responses = []
 
@@ -63,31 +68,43 @@ def following_list_parser(following_list):
     return following
 
 
-def compare_lists(likers, following):
-    to_unfollow = []
-    for account in following:
-        if (account not in likers) and (account not in whitelist):
-            to_unfollow.append(account)
+def compare_lists(post_list, following_list):
+    likers = har_parser(post_list)
+    following = following_list_parser(following_list)
 
-    return to_unfollow
+    filename = time.strftime("%Y%m%d-%H%M%S")
+    h = open(filename, 'w')
+
+    for acc in following:
+        if (acc not in likers) and (acc not in whitelist):
+            h.write(acc)
+            h.write("\n")
+        h.close()
+
+    # to_unfollow = []
+    # for account in following:
+    #     if (account not in likers) and (account not in whitelist):
+    #         to_unfollow.append(account)
+    #
+    # return to_unfollow
 
 
-def generate_file(post_list, following_list):
-    data = compare_lists(har_parser(post_list), following_list_parser(following_list))
-    g = open('to-unfollow.txt', 'w')
-    for account in data:
-        g.write(account)
-        g.write("\n")
-    g.close()
+# def generate_file(post_list, following_list):
+#     data = compare_lists(har_parser(post_list), following_list_parser(following_list))
+#     g = open('to-unfollow.txt', 'w')
+#     for account in data:
+#         g.write(account)
+#         g.write("\n")
+#     g.close()
 
 
 def open_account_tabs(file_name):
     h = open(file_name)
     data = h.readlines()
-    for account in data:
-        webbrowser.open('https://www.instagram.com/' + account)
+    for account_name in data:
+        webbrowser.open('https://www.instagram.com/' + account_name)
+        time.sleep(random.randrange(0, 2))
 
 
-generate_file(posts, following_file)
-# print(whitelist)
+compare_lists(posts, following_file)
 # open_account_tabs('to-unfollow.txt')
