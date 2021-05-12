ca5b96ef7d4194d7f632b8563379e77846277693
as3284 <as3284@cornell.edu>
2021-05-12 00:35:07 -0400

    updating har-parser and adding some sample data

--- har-parser.py
+++ har-parser.py
@@ -4,13 +4,11 @@ import random
 import time
 from os import listdir
 
-posts = listdir("../posts-data")
+# load posts network requests data
+posts = listdir("sample-data/posts-data")
 
-f = open('following-list.txt')
-following_file = f.readlines()
-f.close()
-
-g = open('passlist.txt')
+# load passlist data
+g = open('sample-data/passlist.txt')
 passlist_orig = g.readlines()
 passlist = []
 for account in passlist_orig:
@@ -18,22 +16,26 @@ for account in passlist_orig:
 g.close()
 
 
+# parse post network request data for likers
 def har_parser(post_list):
     likers = []
 
     for post in post_list:
-        with open('posts-data/' + post) as k:
+        with open('sample-data/posts-data/' + post, encoding='utf8', errors='ignore') as k:
             data = json.load(k)
 
         query_responses = []
 
         for entry in data["log"]["entries"]:
             if 'query' in entry["request"]["url"]:
-                query_responses.append(entry)
+                if 'edge_liked_by' in entry["response"]["content"]["text"]:
+                    query_responses.append(entry)
 
         for queryResponse in query_responses:
             for edge in \
-                    json.loads(queryResponse["response"]["content"]["text"])["data"]["shortcode_media"][
+                    json.loads(queryResponse["response"]
+                               ["content"]
+                               ["text"])["data"]["shortcode_media"][
                         "edge_liked_by"][
                         "edges"]:
                 if edge["node"]["username"] not in likers:
@@ -42,54 +44,37 @@ def har_parser(post_list):
     return likers
 
 
-def following_list_parser(following_list):
+# parse data on people I follow
+def following_list_parser():
     following = []
 
-    for line in following_list:
-        if 'profile picture' in line:
-            end_index = line.find("'")
-            following.append(line[:end_index])
+    with open('sample-data/following-list.txt', encoding='utf8') as following_list:
+        for line in following_list:
+            if 'profile picture' in line:
+                following.append(next(following_list).strip())
 
     return following
 
 
-def compare_lists(post_list, following_list):
+# compare likers to following list and create file of accounts to unfollow
+def create_list(post_list, open_tabs=False):
     likers = har_parser(post_list)
-    following = following_list_parser(following_list)
+    following = following_list_parser()
 
-    filename = time.strftime("%Y%m%d-%H%M%S")
-    h = open(filename, 'w')
+    filename = 'sample-data/to-unfollow-' + time.strftime("%Y%m%d-%H%M%S") + '.txt'
+    with open(filename, 'w') as to_unfollow:
+        for acc in following:
+            if (acc not in likers) and (acc not in passlist):
+                to_unfollow.write(acc)
+                to_unfollow.write("\n")
+        to_unfollow.close()
 
-    for acc in following:
-        if (acc not in likers) and (acc not in passlist):
-            h.write(acc)
-            h.write("\n")
-        h.close()
-
-    # to_unfollow = []
-    # for account in following:
-    #     if (account not in likers) and (account not in passlist):
-    #         to_unfollow.append(account)
-    #
-    # return to_unfollow
-
-
-# def generate_file(post_list, following_list):
-#     data = compare_lists(har_parser(post_list), following_list_parser(following_list))
-#     g = open('to-unfollow.txt', 'w')
-#     for account in data:
-#         g.write(account)
-#         g.write("\n")
-#     g.close()
+    if open_tabs:
+        h = open(filename)
+        data = h.readlines()
+        for account_name in data:
+            webbrowser.open('https://www.instagram.com/' + account_name)
+            time.sleep(random.randrange(0, 2))
 
 
-def open_account_tabs(file_name):
-    h = open(file_name)
-    data = h.readlines()
-    for account_name in data:
-        webbrowser.open('https://www.instagram.com/' + account_name)
-        time.sleep(random.randrange(0, 2))
-
-compare_lists(posts, following_file)
-compare_lists(posts, following_file)
-# open_account_tabs('to-unfollow.txt')
+create_list(posts, open_tabs=True)
